13daa0e3b7792482ba8769338347a9bcb76f2c97
Nirvan Tyagi <nirvan.tyagi@gmail.com>
2020-07-23 16:07:42 -0400

    Generic impl of CondSelect for fixed size array

--- r1cs-std/src/select.rs
+++ r1cs-std/src/select.rs
@@ -17,6 +17,26 @@ where
     fn cost() -> usize;
 }
 
+impl<ConstraintF: Field, A: CondSelectGadget<ConstraintF> + Default + Clone> CondSelectGadget<ConstraintF>
+for [A; 32]
+{
+    fn conditionally_select<CS: ConstraintSystem<ConstraintF>>(mut cs: CS, cond: &Boolean, true_value: &[A; 32], false_value: &[A; 32]) -> Result<Self, SynthesisError> {
+        let mut arr = <[A; 32]>::default();
+        arr.clone_from_slice(
+            &true_value.iter().zip(&false_value[..]).enumerate()
+                .map(|(i, (t, f))| A::conditionally_select(&mut cs.ns(|| format!("cond_select_array_{}", i)), cond, t, f))
+                    .collect::<Result<Vec<A>, SynthesisError>>()?[..32]
+        );
+        Ok(arr)
+    }
+
+    fn cost() -> usize {
+        32 * <A as CondSelectGadget<ConstraintF>>::cost()
+    }
+}
+
+
+
 /// Uses two bits to perform a lookup into a table
 pub trait TwoBitLookupGadget<ConstraintF: Field>
 where
